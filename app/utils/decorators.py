417855ddf20a39c132b22cc5c24a0a92ea652afe
rzputradev <rzputradev@gmail.com>
2024-06-26 16:17:30 +0700

    Feat - Add authentication logic

---
+++ app/utils/decorators.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from flask import redirect, url_for
+from flask_login import current_user
+
+def prevent_logged_in_user(view_func):
+    @wraps(view_func)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated:
+            return redirect(url_for('platform.index'))  # Adjust the redirect URL as needed
+        return view_func(*args, **kwargs)
+    return decorated_function
